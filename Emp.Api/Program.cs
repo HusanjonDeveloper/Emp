2948962827ca894e51d041c7922bcad6ffcc9cb1
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-09 15:58:55 +0500

    changed something
    hello

--- Emp.Api/Program.cs
+++ Emp.Api/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Emp.Api
 {
@@ -14,6 +8,7 @@ namespace Emp.Api
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            System.Console.WriteLine("Hello World:");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
